d987fe288246d0014f945fa6e0e44b4fe5dccae3
isimuenterpreneur <isimuenterpreneur@gmail.com>
2026-06-27 09:38:13 +0800

    penghapusan

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: "export", // JANGAN GUNAKAN INI. Ini adalah penyebab utama masalah.
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "https://drive.google.com/file/d/1wgllVj5PnBi7JOxuOe_TTLdiAXgr8I60/view?usp=drive_link",
       },
+      // Tambahkan hostname lain di sini jika Anda menggunakan gambar dari domain lain
     ],
   },
 };
